b88d19e797200a07e02b7822df75a8facf12036c
BranK <23649425+ItsBranK@users.noreply.github.com>
2023-09-11 17:55:44 -0400

    Update Launcher.cr

--- Public/Launcher.cr
+++ Public/Launcher.cr
@@ -1,6 +1,6 @@
 {
     "PsyonixVersion": "230823.66121.430366",
-    "LauncherVersion": "1.2.1",
+    "LauncherVersion": "1.2.2",
     "ModuleVersion": "1.8.1",
     "UninstallerUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v1.8.1/CodeRedUninstaller.zip",
     "LauncherUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v1.8.1/CodeRedLauncher.zip",
@@ -14,7 +14,7 @@
     "ModulesAlt": "null",
     "PropertiesAlt": "null",
     "VariablesAlt": "null",
-    "LauncherChangelog": "The launcher no longer is top most above all other windows when updating.",
+    "LauncherChangelog": "- Remade the launchers UI with a new color scheme, new icons, and added a light mode.\n- The `export crash logs` button in the launcher now also adds your variables file from CodeRed to the zip.\n- Fixed the launcher not loading at all if you tried to run it while not connected to the internet.\n- Fixed the launcher not auto-checking for updates when you enabled it through the checkbox.\n- Fixed the launchers previous and next buttons in the news tab having the wrong icons.",
     "ModuleChangelog": "Added the new Neo Tokyo (Hacked) map to the main menu background changer.\nFixed CodeRed getting stuck thinking you were viewing a replay if you scored a goal and had the disable goal reset mutator enabled.\nFixed a rare crash related to using player anthems from the garage tab that during goal replays.\nFixed the dashboard window becoming semi-invisible if you were playing Rocket League in offline mode.\nUpdated internal SDK to the latest version of Rocket League.\nUpgraded the internal ImGui framework to version 1.89.9.",
     "Credits": "Bakkes, Martinn, TaylorSasser, ButternCream, GlenHumphrey, ToolB0x, lchmagKekse, and Megasplat/Aberinkula/FrancesElMute",
     "Supporters": "Synexe",
