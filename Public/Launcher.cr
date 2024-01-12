0dac2f6f18a532c833b0ac328913dcbb084208e3
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-01-12 12:50:15 -0500

    Update Launcher.cr

--- Public/Launcher.cr
+++ Public/Launcher.cr
@@ -1,6 +1,6 @@
 {
     "PsyonixVersion": "231116.66933.436861",
-    "LauncherVersion": "1.3.1",
+    "LauncherVersion": "1.3.2",
     "ModuleVersion": "1.9.9",
     "UninstallerUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v1.9.9/CodeRedUninstaller.zip",
     "LauncherUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v1.9.9/CodeRedLauncher.zip",
@@ -14,7 +14,7 @@
     "ModulesAlt": "null",
     "PropertiesAlt": "null",
     "VariablesAlt": "null",
-    "LauncherChangelog": "Fixed another bug related to the news article tabs not loading.\nFixed a rare bug that caused checking for an update while actively installing an update.\nInternal improvements and logging for downloading files.",
+    "LauncherChangelog": "Added a new loading screen window when changing install paths, as it may take some time to copy and move everything.\nFixed the user being able to change install paths while CodeRed was injected in the game.\nInternal code refactoring and improvements.",
     "ModuleChangelog": "Added a new ImGui window called `clock`, which displays the current time in the top left of your screen.\nAdded a new setting `imgui_draw_clock`, which automatically opens the new `clock` window on injection.\nAdded a new setting `imgui_clock_24`, which is true by default. If set to false, timestamps will use a 12 hour AM/PM clock instead of a 24 hour one.\nPossibly fixed a random crash that happened if you changed your nickname in the Steam overlay while in a game.\nFixed the game not closing properly if you had the `keys_rage_quits` setting enabled and used `Alt+F4` to close the game.\nFixed a crash that happened if you left a game before it finished completely, or forfeited early.\nFixed the refresh button in the texture browser window not applying the text in the filter box.",
     "Credits": "Bakkes, Martinn, TaylorSasser, ButternCream, GlenHumphrey, ToolB0x, lchmagKekse, and Megasplat/Aberinkula/FrancesElMute",
     "Supporters": "Synexe, MisterDrProf, Th3Ant",
