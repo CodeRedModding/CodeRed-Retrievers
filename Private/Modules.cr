7c5b26d1f7ce56878a01c154a2d7fddecb5e2926
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-04-02 04:40:57 -0400

    Update Modules.cr

---
+++ Private/Modules.cr
@@ -0,0 +1,7 @@
+{
+	"signature": "d7272a7b0b1c3381ec7418f390a1273dbed984c59f5a5edf98ff47328503f53a",
+	"6b8833385417d0e2aa35c0271b7bf58c": "true",
+	"6f7997ddfb09d97522e9340e5288e615": "true",
+	"0014f1d9f7cf29e01f01674025ea8e90": "true",
+	"62db353da608fb6fbbb5de8ef6a598e2": "true"
+}
