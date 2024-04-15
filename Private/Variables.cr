79093821e5daa35ca81bf600ffac7791990d7d9a
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-04-15 13:30:57 -0400

    Update Variables.cr

--- Private/Variables.cr
+++ Private/Variables.cr
@@ -1,4 +1,5 @@
 {
-	"signature": "f9f54742f78e67df10990ee27882a12e796fb43d570d9bcd499df14f0e741d21",
+	"signature": "0555f0a45a848d8f2bf129a831ea96e38e095b74d7b65f4141f242bc99f1ecfa",
+	"b2ef43bdf4356ee931c1d1c250e6539f": "true",
 	"848bbcc3dc3ec2d204443bb3a0f0b944": "false"
 }
