ed84dd9ea6e17811a49edb97ebc71c0a016e267c
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-05-10 15:35:30 -0400

    Update News.cr

--- Public/News.cr
+++ Public/News.cr
@@ -1,1 +1,18 @@
-
+{
+	"url_0": "https://www.rocketleague.com/en/news/may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+	"url_1": "https://www.rocketleague.com/en/news/to-me-my-x-men-mutate-your-ride-in-the-rocket-league-x-men-97-event",
+	"url_2": "https://www.rocketleague.com/en/news/patch-notes-v2-38",
+	"url_3": "https://www.rocketleague.com/en/news/teammate-boost-indicators-and-rarity-name-changes-coming-to-rocket-league",
+	"url_4": "https://www.rocketleague.com/en/news/master-the-elements-avatar-the-last-airbender-boosts-into-rocket-league",
+	"url_5": "https://www.rocketleague.com/en/news/support-your-favorite-season-mode-teams-in-rocket-league-city-slam",
+	"url_6": "https://www.rocketleague.com/en/news/patch-notes-season-14-live",
+	"url_7": "https://www.rocketleague.com/en/news/patch-notes-v2-36",
+	"url_8": "https://www.rocketleague.com/en/news/dive-into-a-fresh-aquadome-arena-in-rocket-league-season-14",
+	"url_9": "https://www.rocketleague.com/en/news/season-13-rewards-feel-the-beat",
+	"url_10": "https://www.rocketleague.com/en/news/keep-a-favorite-extra-mode-in-rotation-with-rocket-leagues-dueling-arcade-playlists",
+	"url_11": "https://www.rocketleague.com/en/news/patch-notes-v2-35",
+	"url_12": "https://www.rocketleague.com/en/news/mandalorian-mayhem-ready-the-hyperdrive-in-rocket-league",
+	"url_13": "https://www.rocketleague.com/en/news/higher-min-level-for-the-competitive-playlist-and-new-esports-shop-items-arrive-in-rocket-league-soon",
+	"url_14": "https://www.rocketleague.com/en/news/pave-your-own-path-with-sovereign-krew-aftermarket-in-rocket-league",
+	"url_15": "https://www.rocketleague.com/en/news/rocket-leagues-frosty-fest-returns-for-2023"
+}
