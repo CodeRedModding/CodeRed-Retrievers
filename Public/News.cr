3acc9805e04414cba624fe4b81211c4778c2e25b
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-05-12 15:19:57 -0400

    Update News.cr

--- Public/News.cr
+++ Public/News.cr
@@ -1,18 +1,84 @@
 {
-	"url": "https://www.rocketleague.com/en/news/may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
-	"url": "https://www.rocketleague.com/en/news/to-me-my-x-men-mutate-your-ride-in-the-rocket-league-x-men-97-event",
-	"url": "https://www.rocketleague.com/en/news/patch-notes-v2-38",
-	"url": "https://www.rocketleague.com/en/news/teammate-boost-indicators-and-rarity-name-changes-coming-to-rocket-league",
-	"url": "https://www.rocketleague.com/en/news/master-the-elements-avatar-the-last-airbender-boosts-into-rocket-league",
-	"url": "https://www.rocketleague.com/en/news/support-your-favorite-season-mode-teams-in-rocket-league-city-slam",
-	"url": "https://www.rocketleague.com/en/news/patch-notes-season-14-live",
-	"url": "https://www.rocketleague.com/en/news/patch-notes-v2-36",
-	"url": "https://www.rocketleague.com/en/news/dive-into-a-fresh-aquadome-arena-in-rocket-league-season-14",
-	"url": "https://www.rocketleague.com/en/news/season-13-rewards-feel-the-beat",
-	"url": "https://www.rocketleague.com/en/news/keep-a-favorite-extra-mode-in-rotation-with-rocket-leagues-dueling-arcade-playlists",
-	"url": "https://www.rocketleague.com/en/news/patch-notes-v2-35",
-	"url": "https://www.rocketleague.com/en/news/mandalorian-mayhem-ready-the-hyperdrive-in-rocket-league",
-	"url": "https://www.rocketleague.com/en/news/higher-min-level-for-the-competitive-playlist-and-new-esports-shop-items-arrive-in-rocket-league-soon",
-	"url": "https://www.rocketleague.com/en/news/pave-your-own-path-with-sovereign-krew-aftermarket-in-rocket-league",
-	"url": "https://www.rocketleague.com/en/news/rocket-leagues-frosty-fest-returns-for-2023"
+  "news": [
+    {
+      "title": "May the 4th Be with You in Rocket League - Celebrate Star Wars Day 2024!",
+      "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+      "imageUrl": "https://i.imgur.com/poV804a.png"
+    },
+    {
+      "title": "To Me, My X-Men! Mutate Your Ride in the Rocket League | X-Men ‘97 Event",
+      "slug": "to-me-my-x-men-mutate-your-ride-in-the-rocket-league-x-men-97-event",
+      "imageUrl": "https://i.imgur.com/mQaoEIP.png"
+    }
+    {
+      "title": "Patch Notes v2.38",
+      "slug": "patch-notes-v2-38",
+      "imageUrl": "https://i.imgur.com/s6GBddQ.png"
+    },
+    {
+      "title": "Teammate Boost Indicators + Rarity Name Changes Coming to Rocket League",
+      "slug": "teammate-boost-indicators-and-rarity-name-changes-coming-to-rocket-league",
+      "imageUrl": "https://i.imgur.com/JdxZOx8.png"
+    },
+    {
+      "title": "Master the Elements - Avatar: The Last Airbender Boosts into Rocket League!",
+      "slug": "master-the-elements-avatar-the-last-airbender-boosts-into-rocket-league",
+      "imageUrl": "https://i.imgur.com/KVTc0Ag.png"
+    },
+    {
+      "title": "Support Your Favorite Season Mode Teams in Rocket League City Slam!",
+      "slug": "support-your-favorite-season-mode-teams-in-rocket-league-city-slam",
+      "imageUrl": "https://i.imgur.com/8DSirJN.png"
+    },
+    {
+      "title": "Patch Notes: Season 14 Live",
+      "slug": "patch-notes-season-14-live",
+      "imageUrl": "https://i.imgur.com/BXKwExd.png"
+    }
+    {
+      "title": "Patch Notes v2.36",
+      "slug": "patch-notes-v2-36",
+      "imageUrl": "https://i.imgur.com/Onbb7pk.png"
+    },
+    {
+      "title": "Dive into a Fresh AquaDome Arena in Rocket League Season 14!",
+      "slug": "dive-into-a-fresh-aquadome-arena-in-rocket-league-season-14",
+      "imageUrl": "https://i.imgur.com/yhFKctF.png"
+    },
+    {
+      "title": "Season 13 Rewards Feel the Beat",
+      "slug": "season-13-rewards-feel-the-beat",
+      "imageUrl": "https://i.imgur.com/JWVLoSr.png"
+    },
+    {
+      "title": "May the 4th Be with You in Rocket League - Celebrate Star Wars Day 2024!",
+      "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+      "imageUrl": "https://i.imgur.com/poV804a.png"
+    },
+    {
+      "title": "May the 4th Be with You in Rocket League - Celebrate Star Wars Day 2024!",
+      "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+      "imageUrl": "https://i.imgur.com/poV804a.png"
+    }
+    {
+      "title": "May the 4th Be with You in Rocket League - Celebrate Star Wars Day 2024!",
+      "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+      "imageUrl": "https://i.imgur.com/poV804a.png"
+    },
+    {
+      "title": "May the 4th Be with You in Rocket League - Celebrate Star Wars Day 2024!",
+      "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+      "imageUrl": "https://i.imgur.com/poV804a.png"
+    },
+    {
+      "title": "May the 4th Be with You in Rocket League - Celebrate Star Wars Day 2024!",
+      "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+      "imageUrl": "https://i.imgur.com/poV804a.png"
+    },
+    {
+      "title": "May the 4th Be with You in Rocket League - Celebrate Star Wars Day 2024!",
+      "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
+      "imageUrl": "https://i.imgur.com/poV804a.png"
+    },
+  ]
 }
