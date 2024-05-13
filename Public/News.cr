86cdddd8d84cd2e7bfefcada72a3c6d6023ce6ac
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-05-13 15:28:22 +0000

    Update News.cr

--- Public/News.cr
+++ Public/News.cr
@@ -10,77 +10,107 @@
     {
       "title": "To Me, My X-Men! Mutate Your Ride in the Rocket League | X-Men ‘97 Event",
       "slug": "to-me-my-x-men-mutate-your-ride-in-the-rocket-league-x-men-97-event",
-      "imageUrl": "https://i.imgur.com/mQaoEIP.png"
+      "imageUrl": "https://i.imgur.com/mQaoEIP.png",
+      "author": "Psyonix Team",
+      "date": "2024-04-20"
     },
     {
       "title": "Patch Notes v2.38",
       "slug": "patch-notes-v2-38",
-      "imageUrl": "https://i.imgur.com/s6GBddQ.png"
+      "imageUrl": "https://i.imgur.com/s6GBddQ.png",
+      "author": "Devin Connors",
+      "date": "2024-04-16"
     },
     {
       "title": "Teammate Boost Indicators + Rarity Name Changes Coming to Rocket League",
       "slug": "teammate-boost-indicators-and-rarity-name-changes-coming-to-rocket-league",
-      "imageUrl": "https://i.imgur.com/JdxZOx8.png"
+      "imageUrl": "https://i.imgur.com/JdxZOx8.png",
+      "author": "Psyonix Team",
+      "date": "2024-04-15"
     },
     {
       "title": "Master the Elements - Avatar: The Last Airbender Boosts into Rocket League!",
       "slug": "master-the-elements-avatar-the-last-airbender-boosts-into-rocket-league",
-      "imageUrl": "https://i.imgur.com/KVTc0Ag.png"
+      "imageUrl": "https://i.imgur.com/KVTc0Ag.png",
+      "author": "Psyonix Team",
+      "date": "2024-04-11"
     },
     {
       "title": "Support Your Favorite Season Mode Teams in Rocket League City Slam!",
       "slug": "support-your-favorite-season-mode-teams-in-rocket-league-city-slam",
-      "imageUrl": "https://i.imgur.com/8DSirJN.png"
+      "imageUrl": "https://i.imgur.com/8DSirJN.png",
+      "author": "Psyonix Team",
+      "date": "2024-03-23"
     },
     {
       "title": "Patch Notes: Season 14 Live",
       "slug": "patch-notes-season-14-live",
-      "imageUrl": "https://i.imgur.com/BXKwExd.png"
+      "imageUrl": "https://i.imgur.com/BXKwExd.png",
+      "author": "Devin Connors",
+      "date": "2024-03-06"
     },
     {
       "title": "Patch Notes v2.36",
       "slug": "patch-notes-v2-36",
-      "imageUrl": "https://i.imgur.com/Onbb7pk.png"
+      "imageUrl": "https://i.imgur.com/Onbb7pk.png",
+      "author": "Devin Connors",
+      "date": "2024-03-05"
     },
     {
       "title": "Dive into a Fresh AquaDome Arena in Rocket League Season 14!",
       "slug": "dive-into-a-fresh-aquadome-arena-in-rocket-league-season-14",
-      "imageUrl": "https://i.imgur.com/yhFKctF.png"
+      "imageUrl": "https://i.imgur.com/yhFKctF.png",
+      "author": "Psyonix Team",
+      "date": "2024-03-04"
     },
     {
       "title": "Season 13 Rewards Feel the Beat",
       "slug": "season-13-rewards-feel-the-beat",
-      "imageUrl": "https://i.imgur.com/JWVLoSr.png"
+      "imageUrl": "https://i.imgur.com/JWVLoSr.png",
+      "author": "Laudie Gonzalez",
+      "date": "2024-02-23"
     },
     {
       "title": "Keep a Favorite Extra Mode in Rotation with Rocket League’s Dueling Arcade Playlists",
       "slug": "keep-a-favorite-extra-mode-in-rotation-with-rocket-leagues-dueling-arcade-playlists",
-      "imageUrl": "https://i.imgur.com/ZuHdSr6.png"
+      "imageUrl": "https://i.imgur.com/ZuHdSr6.png",
+      "author": "Psyonix Team",
+      "date": "2024-02-21"
     },
     {
       "title": "Rocket League Patch Notes v2.35",
       "slug": "patch-notes-v2-35",
-      "imageUrl": "https://i.imgur.com/fJOwJhr.png"
+      "imageUrl": "https://i.imgur.com/fJOwJhr.png",
+      "author": "Laudie Gonzalez",
+      "date": "2024-02-06"
     },
     {
       "title": "Mandalorian Mayhem: Ready the Hyperdrive in Rocket League!",
       "slug": "mandalorian-mayhem-ready-the-hyperdrive-in-rocket-league",
-      "imageUrl": "https://i.imgur.com/tEHkuqA.png"
+      "imageUrl": "https://i.imgur.com/tEHkuqA.png",
+      "author": "Psyonix Team",
+      "date": "2024-02-06"
     },
     {
       "title": "Higher Min Level for the Competitive Playlist and New Esports Shop Items Arrive in Rocket League Soon",
       "slug": "may-the-4th-be-with-you-in-rocket-league-celebrate-star-wars-day-2024",
-      "imageUrl": "https://i.imgur.com/oqITHLU.png"
+      "imageUrl": "https://i.imgur.com/oqITHLU.png",
+      "author": "Psyonix Team",
+      "date": "2024-02-05"
     },
     {
       "title": "Pave Your Own Path with Sovereign Krew: Aftermarket in Rocket League",
       "slug": "pave-your-own-path-with-sovereign-krew-aftermarket-in-rocket-league",
-      "imageUrl": "https://i.imgur.com/ZG9v8V9.png"
+      "imageUrl": "https://i.imgur.com/ZG9v8V9.png",
+      "author": "Psyonix Team",
+      "date": "2024-01-11"
     },
     {
       "title": "Rocket League’s Frosty Fest Returns for 2023!",
       "slug": "rocket-leagues-frosty-fest-returns-for-2023",
-      "imageUrl": "https://i.imgur.com/ucdI3fv.png"
+      "imageUrl": "https://i.imgur.com/ucdI3fv.png",
+      "author": "Psyonix Team",
+      "date": "2023-12-13"
     }
   ]
 }
