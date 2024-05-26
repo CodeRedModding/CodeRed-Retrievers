9de3a38a6a2a17473ac023bbc6e8dbb6c9bdf230
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-05-26 17:08:04 -0400

    Initial Commit

--- Private/Titles.cr
+++ Private/Titles.cr
@@ -1,6 +1,84 @@
 {
-	"signature": "bf08b6eb03cabd132fc34d4486fb0e40e38a7961a85ab46bfe12d816059863d6",
+	"signature": "2800d4d72cecd22aaffcff1aedad4bdc6cca4a02420d7aa895acecc06f2d84a1",
 	"titles": [
+		{
+			"id": "Q1JfU3RhZmY=",
+			"text": "Q29kZVJlZCBEaXNjb3JkIFN0YWZm",
+			"text_color": "MTY3MjQ1MTc=",
+			"glow_color": "MTY3MjgzNzY="
+		},
+		{
+			"id": "Q1JfQm91bnR5X0h1bnRlcg==",
+			"text": "SGFja2VyT25lIEJvdW50eSBIdW50ZXI=",
+			"text_color": "MTY3NzcyMTU=",
+			"glow_color": "MTY3NzcyMTU="
+		},
+		{
+			"id": "Q1JfQ291bnRlcg==",
+			"text": "Q29kZVJlZCBDb3VudGluZyBDaGFtcGlvbg==",
+			"text_color": "MA==",
+			"glow_color": "MA=="
+		},
+		{
+			"id": "Q1JfQm9vc3Rlcg==",
+			"text": "Q29kZVJlZCBTZXJ2ZXIgQm9vc3Rlcg==",
+			"text_color": "MTY3MzYyNDU=",
+			"glow_color": "MTY3MTE5MDU="
+		},
+		{
+			"id": "Q1JfU3VwcG9ydGVy",
+			"text": "Q29kZVJlZCBTdXBwb3J0ZXI=",
+			"text_color": "MjAyNzc3NQ==",
+			"glow_color": "NjgxMzE4Mw=="
+		},
+		{
+			"id": "Q1JfQmV0YV9UZXN0ZXI=",
+			"text": "Q29kZVJlZCBCZXRhIFRlc3Rlcg==",
+			"text_color": "NTkzMDQ5NQ==",
+			"glow_color": "MTI2Mjg0NQ=="
+		},
+		{
+			"id": "Q1JfRWRpdA==",
+			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
+			"text_color": "MA==",
+			"glow_color": "MA=="
+		},
+		{
+			"id": "Q1JfQWxwaGFfVGVzdGVy",
+			"text": "Q29kZVJlZCBBbHBoYSBUZXN0ZXI=",
+			"text_color": "MTI3OTc2OTA=",
+			"glow_color": "MTE3OTY3MzU="
+		},
+		{
+			"id": "Q1JfQWxs",
+			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
+			"text_color": "MA==",
+			"glow_color": "MA=="
+		},
+		{
+			"id": "Q1JfRmVtYm95",
+			"text": "RmVtYm95IEVudGh1c2lhc3Q=",
+			"text_color": "MTM2NzI3NQ==",
+			"glow_color": "OTU2MTg1MA=="
+		},
+		{
+			"id": "Q1JfR2lybF9LaXNzZXI=",
+			"text": "VmVyaWZpZWQgR2lybCBLaXNzZXI=",
+			"text_color": "MTY3MzYyNDU=",
+			"glow_color": "MTY3MTE5MDU="
+		},
+		{
+			"id": "Q1JfQ29ubmVjdA==",
+			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
+			"text_color": "MA==",
+			"glow_color": "MA=="
+		},
+		{
+			"id": "Q1JfRHVtcGVy",
+			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
+			"text_color": "MA==",
+			"glow_color": "MA=="
+		},
 		{
 			"id": "Q1JfQm95X0tpc3Nlcg==",
 			"text": "VmVyaWZpZWQgQm95IEtpc3Nlcg==",
@@ -14,94 +92,16 @@
 			"glow_color": "MTY3MTE5MDU="
 		},
 		{
-			"id": "Q1JfQWxs",
-			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
-			"text_color": "MA==",
-			"glow_color": "MA=="
-		},
-		{
-			"id": "Q1JfRWRpdA==",
-			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
-			"text_color": "MA==",
-			"glow_color": "MA=="
-		},
-		{
-			"id": "Q1JfR2lybF9LaXNzZXI=",
-			"text": "VmVyaWZpZWQgR2lybCBLaXNzZXI=",
-			"text_color": "MTY3MzYyNDU=",
-			"glow_color": "MTY3MTE5MDU="
-		},
-		{
-			"id": "Q1JfQ291bnRlcg==",
-			"text": "Q29kZVJlZCBDb3VudGluZyBDaGFtcGlvbg==",
-			"text_color": "MA==",
-			"glow_color": "MA=="
-		},
-		{
-			"id": "Q1JfRnVycnk=",
-			"text": "U3VzcGljaW91c2x5IExld2QgRnVycnk=",
-			"text_color": "MTY3MzYyNDU=",
-			"glow_color": "MTY3MTE5MDU="
-		},
-		{
-			"id": "Q1JfQ29ubmVjdA==",
-			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
-			"text_color": "MA==",
-			"glow_color": "MA=="
-		},
-		{
-			"id": "Q1JfRmVtYm95",
-			"text": "RmVtYm95IEVudGh1c2lhc3Q=",
-			"text_color": "MTM2NzI3NQ==",
-			"glow_color": "OTU2MTg1MA=="
-		},
-		{
-			"id": "Q1JfQWxwaGFfVGVzdGVy",
-			"text": "Q29kZVJlZCBBbHBoYSBUZXN0ZXI=",
-			"text_color": "MTI3OTc2OTA=",
-			"glow_color": "MTE3OTY3MzU="
-		},
-		{
-			"id": "Q1JfQm9vc3Rlcg==",
-			"text": "Q29kZVJlZCBTZXJ2ZXIgQm9vc3Rlcg==",
-			"text_color": "MTY3MzYyNDU=",
-			"glow_color": "MTY3MTE5MDU="
-		},
-		{
-			"id": "Q1JfRHVtcGVy",
-			"text": "UGxhY2Vob2xkZXIgVGV4dA==",
-			"text_color": "MA==",
-			"glow_color": "MA=="
-		},
-		{
-			"id": "Q1JfU3RhZmY=",
-			"text": "Q29kZVJlZCBEaXNjb3JkIFN0YWZm",
-			"text_color": "MTY3MjQ1MTc=",
-			"glow_color": "MTY3MjgzNzY="
-		},
-		{
-			"id": "Q1JfU3VwcG9ydGVy",
-			"text": "Q29kZVJlZCBTdXBwb3J0ZXI=",
-			"text_color": "MjAyNzc3NQ==",
-			"glow_color": "NjgxMzE4Mw=="
-		},
-		{
 			"id": "Q1JfUXVlZXI=",
 			"text": "QWxwaGFiZXQgTWFmaWEgTWVtYmVy",
 			"text_color": "MTE0NDI2ODc=",
 			"glow_color": "ODgwMDI1NQ=="
 		},
 		{
-			"id": "Q1JfQmV0YV9UZXN0ZXI=",
-			"text": "Q29kZVJlZCBCZXRhIFRlc3Rlcg==",
-			"text_color": "NTkzMDQ5NQ==",
-			"glow_color": "MTI2Mjg0NQ=="
-		},
-		{
-			"id": "Q1JfQm91bnR5X0h1bnRlcg==",
-			"text": "SGFja2VyT25lIEJvdW50eSBIdW50ZXI=",
-			"text_color": "MTY3NzcyMTU=",
-			"glow_color": "MTY3NzcyMTU="
+			"id": "Q1JfRnVycnk=",
+			"text": "U3VzcGljaW91c2x5IExld2QgRnVycnk=",
+			"text_color": "MTY3MzYyNDU=",
+			"glow_color": "MTY3MTE5MDU="
 		}
 	]
 }
