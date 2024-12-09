c2fa67297d84f3e8772ca48212d92db264f45e55
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-12-09 14:03:36 -0500

    Initial Commit

--- Public/Launcher.cr
+++ Public/Launcher.cr
@@ -1,20 +1,17 @@
 {
-	"signature": "2715eeaf385a67f24361d2fa208f3293200f444227f8e3df054fda15913e1146e01c35182bdec569ac13ba5dc0d7a787fd97d97ea94e656e5de7e14a02e7d7cb",
+	"signature": "6925c40d1205a94708adaa754c969de9b46d5bade13ac827ac88755df487a78630d31a5bde9ae5ff0af92aff0534baa2fabf98feeef6bad8c8cf41c02dc65d72",
 	"LauncherAlt": "null",
-	"LauncherChangelog": "Added more detailed info to the update window when installing updates.\nChanged some error messages and logs to provide better info if something is blocking the launchers permissions.",
-	"Supporters": "Synexe, MisterDrProf, Th3Ant, Anga, FREE AOL, Mr Reiner",
-	"UninstallerUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedUninstaller.zip",
-	"Credits": "Bakkes, Martinn, TaylorSasser, ButternCream, GlenHumphrey, Segal, ToolB0x, lchmagKekse, and Megasplat/Aberinkula/FrancesElMute",
 	"PsyonixVersion": "241120.79850.467296",
-	"DropperUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedDropper.zip",
-	"DiscordUrl": "https://discord.gg/TnmF5MhRak",
-	"AltEndpoint": "false",
 	"ModuleUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedModule.zip",
-	"WebsiteUrl": "https://coderedmodding.github.io/",
-	"LauncherVersion": "1.4.0",
 	"KofiUrl": "https://ko-fi.com/coderedmodding/",
-	"LauncherUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedLauncher.zip",
+	"LauncherChangelog": "Added more detailed info to the update window when installing updates.\nChanged some error messages and logs to provide better info if something is blocking the launchers permissions.",
+	"UninstallerUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedUninstaller.zip",
 	"ModuleVersion": "2.4.4",
+	"WebsiteUrl": "https://coderedmodding.github.io/",
+	"Supporters": "Synexe, MisterDrProf, Th3Ant, Anga, FREE AOL, Mr Reiner",
+	"NewsUrl": "https://www.rocketleague.com/en/news/",
+	"LauncherVersion": "1.4.0",
+	"LauncherUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedLauncher.zip",
 	"ModuleChangelog": "Added a new setting `disable_epic_overlay` which disables the new Epic Games overlay, this works for both Steam and Epic.
 Added the new `Shokunin` car bodies to the anonymizer setting `garage_hide_loadouts`.
 Changed the `ImGui.ini` file to be stored in `CodeRedSettings` as opposed to the games directory.
@@ -23,5 +20,8 @@ Fixed duplicate spawn particles appearing on your car when applying CodeRed load
 Fixed the disable boost pads settings not working properly as of the last game update
 Fixed custom post process effects not working on some main menu backgrounds.
 Improvements and bug fixes to ImGui related to hooks and global scaling.",
-	"NewsUrl": "https://www.rocketleague.com/en/news/"
+	"DropperUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedDropper.zip",
+	"DiscordUrl": "https://discord.gg/v2DP7pQG",
+	"Credits": "Bakkes, Martinn, TaylorSasser, ButternCream, GlenHumphrey, Segal, ToolB0x, lchmagKekse, and Megasplat/Aberinkula/FrancesElMute",
+	"AltEndpoint": "false"
 }
