270a8f6050d67ce16012da3efbc9c8c0010b8811
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-12-09 14:05:55 -0500

    Initial Commit

--- Public/Launcher.cr
+++ Public/Launcher.cr
@@ -1,17 +1,12 @@
 {
-	"signature": "6925c40d1205a94708adaa754c969de9b46d5bade13ac827ac88755df487a78630d31a5bde9ae5ff0af92aff0534baa2fabf98feeef6bad8c8cf41c02dc65d72",
-	"LauncherAlt": "null",
-	"PsyonixVersion": "241120.79850.467296",
-	"ModuleUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedModule.zip",
-	"KofiUrl": "https://ko-fi.com/coderedmodding/",
-	"LauncherChangelog": "Added more detailed info to the update window when installing updates.\nChanged some error messages and logs to provide better info if something is blocking the launchers permissions.",
-	"UninstallerUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedUninstaller.zip",
-	"ModuleVersion": "2.4.4",
-	"WebsiteUrl": "https://coderedmodding.github.io/",
-	"Supporters": "Synexe, MisterDrProf, Th3Ant, Anga, FREE AOL, Mr Reiner",
+	"signature": "a363d9fab5dd26481db3a4ba957fbf25e87e361b17777a07532c30e08dd660f4d7bc5bc85363e6b0f5c92e69224f767a28c3086490788773b254e8ed3308af2d",
+	"DropperUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedDropper.zip",
 	"NewsUrl": "https://www.rocketleague.com/en/news/",
+	"KofiUrl": "https://ko-fi.com/coderedmodding/",
+	"UninstallerUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedUninstaller.zip",
+	"Supporters": "Synexe, MisterDrProf, Th3Ant, Anga, FREE AOL, Mr Reiner",
+	"LauncherAlt": "null",
 	"LauncherVersion": "1.4.0",
-	"LauncherUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedLauncher.zip",
 	"ModuleChangelog": "Added a new setting `disable_epic_overlay` which disables the new Epic Games overlay, this works for both Steam and Epic.
 Added the new `Shokunin` car bodies to the anonymizer setting `garage_hide_loadouts`.
 Changed the `ImGui.ini` file to be stored in `CodeRedSettings` as opposed to the games directory.
@@ -20,8 +15,13 @@ Fixed duplicate spawn particles appearing on your car when applying CodeRed load
 Fixed the disable boost pads settings not working properly as of the last game update
 Fixed custom post process effects not working on some main menu backgrounds.
 Improvements and bug fixes to ImGui related to hooks and global scaling.",
-	"DropperUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedDropper.zip",
-	"DiscordUrl": "https://discord.gg/v2DP7pQG",
+	"LauncherUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedLauncher.zip",
+	"ModuleUrl": "https://github.com/CodeRedModding/CodeRed-Launcher/releases/download/v2.4.4/CodeRedModule.zip",
+	"WebsiteUrl": "https://coderedmodding.github.io/",
+	"LauncherChangelog": "Added more detailed info to the update window when installing updates.\nChanged some error messages and logs to provide better info if something is blocking the launchers permissions.",
+	"DiscordUrl": "https://discord.gg/WzE8fbv5PR",
+	"ModuleVersion": "2.4.4",
+	"AltEndpoint": "false",
 	"Credits": "Bakkes, Martinn, TaylorSasser, ButternCream, GlenHumphrey, Segal, ToolB0x, lchmagKekse, and Megasplat/Aberinkula/FrancesElMute",
-	"AltEndpoint": "false"
+	"PsyonixVersion": "241120.79850.467296"
 }
