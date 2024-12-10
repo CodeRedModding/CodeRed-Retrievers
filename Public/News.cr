c643926392824774069d107ac6b05c9438211bf6
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-12-09 21:01:17 -0500

    Initial Commit

--- Public/News.cr
+++ Public/News.cr
@@ -28,7 +28,7 @@
       "title": "Collegiate Rocket League Spring 2025 Preview",
       "slug": "collegiate-rocket-league-spring-2025-preview",
 	  "category": "Esports News",
-      "imageUrl": "https://i.imgur.com/WccRFiS.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/collegiate-rocket-league-spring-2025-preview.jpg",
       "author": "Psyonix Team",
       "date": "2024-11-22"
     },
@@ -36,7 +36,7 @@
       "title": "Rocket League S16’s Season Rush Kicks Off!",
       "slug": "rocket-league-s16-season-rush-kicks-off",
 	  "category": "Game News",
-      "imageUrl": "https://i.imgur.com/bbzSViI.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/rocket-league-s16-season-rush-kicks-off.jpg",
       "author": "Psyonix Team",
       "date": "2024-11-19"
     },
@@ -44,7 +44,7 @@
       "title": "RLCS Is Back With a Bang For 2025!",
       "slug": "rlcs-is-back-with-a-bang-for-2025",
 	  "category": "Esports News",
-      "imageUrl": "https://i.imgur.com/TBFAjg4.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/rlcs-is-back-with-a-bang-for-2025.jpg",
       "author": "Psyonix Team",
       "date": "2024-10-23"
     },
@@ -52,7 +52,7 @@
       "title": "Patch Notes v2.45",
       "slug": "patch-notes-v2-45",
 	  "category": "Patch Notes",
-      "imageUrl": "https://i.imgur.com/n1KLmTa.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-45.jpg",
       "author": "Devin Conners",
       "date": "2024-10-22"
     },
@@ -60,7 +60,7 @@
       "title": "Introducing Rocket League Match History and Player Profiles!",
       "slug": "introducing-rocket-league-match-history-and-player-profiles",
 	  "category": "Game News",
-      "imageUrl": "https://i.imgur.com/jiZzs7Z.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/introducing-rocket-league-match-history-and-player-profiles.jpg",
       "author": "Psyonix Team",
       "date": "2024-10-21"
     },
@@ -68,7 +68,7 @@
       "title": "The 2024 RLCS Offseason Heats Up With More Ways to Compete!",
       "slug": "2024-offseason-tournaments",
 	  "category": "Esports News",
-      "imageUrl": "https://i.imgur.com/jWsiPIB.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/2024-offseason-tournaments.jpg",
       "author": "Psyonix Team",
       "date": "2024-10-16"
     },
@@ -76,7 +76,7 @@
       "title": "It's a Spider-Man Spooktacular in Rocket League's Haunted Hallows!",
       "slug": "its-a-spider-man-spooktacular-in-rocket-leagues-haunted-hallows",
 	  "category": "Game News",
-      "imageUrl": "https://i.imgur.com/mECM0V6.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/its-a-spider-man-spooktacular-in-rocket-leagues-haunted-hallows.jpg",
       "author": "Psyonix Team",
       "date": "2024-10-16"
     },
@@ -84,7 +84,7 @@
       "title": "Patch Notes v2.44",
       "slug": "patch-notes-v2-44",
 	  "category": "Patch Notes",
-      "imageUrl": "https://i.imgur.com/VIDOPDy.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-44.jpg",
       "author": "Devin Conners",
       "date": "2024-09-24"
     },
@@ -92,7 +92,7 @@
       "title": "Collegiate Rocket League Returns For Fall 2024!",
       "slug": "collegiate-rocket-league-returns-for-fall-2024",
 	  "category": "Esports News",
-      "imageUrl": "https://i.imgur.com/7HLBXII.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/collegiate-rocket-league-returns-for-fall-2024.jpg",
       "author": "Psyonix Team",
       "date": "2024-09-15"
     },
@@ -100,7 +100,7 @@
       "title": "Upcoming Rocket League Pricing Alignment in Africa, Asia, Oceania, and South America in October 2024",
       "slug": "upcoming-rocket-league-pricing-alignment-in-africa-asia-oceania-and-south-america-in-october-2024",
 	  "category": "Game News",
-      "imageUrl": "https://i.imgur.com/Ch5M5hT.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/upcoming-rocket-league-pricing-alignment-in-africa-asia-oceania-and-south-america-in-october-2024.jpg",
       "author": "Psyonix Team",
       "date": "2024-09-10"
     },
@@ -108,7 +108,7 @@
       "title": "2024 Rocket League World Championship Primer + On-Site Guide",
       "slug": "2024-rlcs-world-championship-primer-plus-onsite-guide",
 	  "category": "Esports News",
-      "imageUrl": "https://i.imgur.com/jVFngxb.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/2024-rlcs-world-championship-primer-plus-onsite-guide.jpg",
       "author": "Psyonix Team",
       "date": "2024-09-09"
     },
@@ -116,7 +116,7 @@
       "title": "Champions Road Returns to Rocket League in 2024",
       "slug": "champions-road-returns-to-rocket-league-in-2024",
 	  "category": "Game News",
-      "imageUrl": "https://i.imgur.com/ooXWDQl.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/champions-road-returns-to-rocket-league-in-2024.jpg",
       "author": "Psyonix Team",
       "date": "2024-09-06"
     },
@@ -124,7 +124,7 @@
       "title": "Patch Notes: Season 16 Live",
       "slug": "patch-notes-season-16-live",
 	  "category": "Patch Notes",
-      "imageUrl": "https://i.imgur.com/803V68D.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-season-16-live.jpg",
       "author": "Devin Connors",
       "date": "2024-09-04"
     },
@@ -132,7 +132,7 @@
       "title": "Patch Notes v2.43",
       "slug": "patch-notes-v2-43",
 	  "category": "Game Update",
-      "imageUrl": "https://i.imgur.com/w128Efw.png",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-43.jpg",
       "author": "Devin Connors",
       "date": "2024-09-03"
     }
