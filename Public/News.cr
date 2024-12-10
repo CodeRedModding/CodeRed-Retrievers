0e37463f6f8262c383fb8a66dc1a19510fe771e2
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-12-09 20:58:07 -0500

    Initial Commit

--- Public/News.cr
+++ Public/News.cr
@@ -3,6 +3,7 @@
     {
       "title": "Patch Notes: Season 17 Live",
       "slug": "patch-notes-season-17-live",
+	  "category": "Patch Notes",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-season-17-live.jpg",
       "author": "Laudie Gonzalez",
       "date": "2024-12-04"
@@ -10,20 +11,23 @@
     {
       "title": "Rocket League Patch Notes v2.46",
       "slug": "patch-notes-v2-46",
-      "imageUrl": "https://i.imgur.com/8z9DpBI.png",
+	  "category": "Game Update",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-46.jpg",
       "author": "Laudie Gonzalez",
       "date": "2024-12-03"
     },
     {
       "title": "Season 17 Brings Online Free Play as Rocket League Heads to the Arcades",
       "slug": "season-17-brings-online-free-play-as-rocket-league-heads-to-the-arcades",
-      "imageUrl": "https://i.imgur.com/36cGzoM.png",
+	  "category": "Game News",
+      "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/season-17-brings-online-free-play-as-rocket-league-heads-to-the-arcades.jpg",
       "author": "Psyonix Team",
       "date": "2024-11-26"
     },
     {
       "title": "Collegiate Rocket League Spring 2025 Preview",
       "slug": "collegiate-rocket-league-spring-2025-preview",
+	  "category": "Esports News",
       "imageUrl": "https://i.imgur.com/WccRFiS.png",
       "author": "Psyonix Team",
       "date": "2024-11-22"
@@ -31,6 +35,7 @@
     {
       "title": "Rocket League S16’s Season Rush Kicks Off!",
       "slug": "rocket-league-s16-season-rush-kicks-off",
+	  "category": "Game News",
       "imageUrl": "https://i.imgur.com/bbzSViI.png",
       "author": "Psyonix Team",
       "date": "2024-11-19"
@@ -38,6 +43,7 @@
     {
       "title": "RLCS Is Back With a Bang For 2025!",
       "slug": "rlcs-is-back-with-a-bang-for-2025",
+	  "category": "Esports News",
       "imageUrl": "https://i.imgur.com/TBFAjg4.png",
       "author": "Psyonix Team",
       "date": "2024-10-23"
@@ -45,6 +51,7 @@
     {
       "title": "Patch Notes v2.45",
       "slug": "patch-notes-v2-45",
+	  "category": "Patch Notes",
       "imageUrl": "https://i.imgur.com/n1KLmTa.png",
       "author": "Devin Conners",
       "date": "2024-10-22"
@@ -52,6 +59,7 @@
     {
       "title": "Introducing Rocket League Match History and Player Profiles!",
       "slug": "introducing-rocket-league-match-history-and-player-profiles",
+	  "category": "Game News",
       "imageUrl": "https://i.imgur.com/jiZzs7Z.png",
       "author": "Psyonix Team",
       "date": "2024-10-21"
@@ -59,13 +67,15 @@
     {
       "title": "The 2024 RLCS Offseason Heats Up With More Ways to Compete!",
       "slug": "2024-offseason-tournaments",
+	  "category": "Esports News",
       "imageUrl": "https://i.imgur.com/jWsiPIB.png",
       "author": "Psyonix Team",
       "date": "2024-10-16"
     },
     {
       "title": "It's a Spider-Man Spooktacular in Rocket League's Haunted Hallows!",
-      "slug": "its-a-spider-man-spooktacular-in-rocket-leagues-haunted-hallows",Cadillac 
+      "slug": "its-a-spider-man-spooktacular-in-rocket-leagues-haunted-hallows",
+	  "category": "Game News",
       "imageUrl": "https://i.imgur.com/mECM0V6.png",
       "author": "Psyonix Team",
       "date": "2024-10-16"
@@ -73,6 +83,7 @@
     {
       "title": "Patch Notes v2.44",
       "slug": "patch-notes-v2-44",
+	  "category": "Patch Notes",
       "imageUrl": "https://i.imgur.com/VIDOPDy.png",
       "author": "Devin Conners",
       "date": "2024-09-24"
@@ -80,6 +91,7 @@
     {
       "title": "Collegiate Rocket League Returns For Fall 2024!",
       "slug": "collegiate-rocket-league-returns-for-fall-2024",
+	  "category": "Esports News",
       "imageUrl": "https://i.imgur.com/7HLBXII.png",
       "author": "Psyonix Team",
       "date": "2024-09-15"
@@ -87,6 +99,7 @@
     {
       "title": "Upcoming Rocket League Pricing Alignment in Africa, Asia, Oceania, and South America in October 2024",
       "slug": "upcoming-rocket-league-pricing-alignment-in-africa-asia-oceania-and-south-america-in-october-2024",
+	  "category": "Game News",
       "imageUrl": "https://i.imgur.com/Ch5M5hT.png",
       "author": "Psyonix Team",
       "date": "2024-09-10"
@@ -94,6 +107,7 @@
     {
       "title": "2024 Rocket League World Championship Primer + On-Site Guide",
       "slug": "2024-rlcs-world-championship-primer-plus-onsite-guide",
+	  "category": "Esports News",
       "imageUrl": "https://i.imgur.com/jVFngxb.png",
       "author": "Psyonix Team",
       "date": "2024-09-09"
@@ -101,6 +115,7 @@
     {
       "title": "Champions Road Returns to Rocket League in 2024",
       "slug": "champions-road-returns-to-rocket-league-in-2024",
+	  "category": "Game News",
       "imageUrl": "https://i.imgur.com/ooXWDQl.png",
       "author": "Psyonix Team",
       "date": "2024-09-06"
@@ -108,6 +123,7 @@
     {
       "title": "Patch Notes: Season 16 Live",
       "slug": "patch-notes-season-16-live",
+	  "category": "Patch Notes",
       "imageUrl": "https://i.imgur.com/803V68D.png",
       "author": "Devin Connors",
       "date": "2024-09-04"
@@ -115,6 +131,7 @@
     {
       "title": "Patch Notes v2.43",
       "slug": "patch-notes-v2-43",
+	  "category": "Game Update",
       "imageUrl": "https://i.imgur.com/w128Efw.png",
       "author": "Devin Connors",
       "date": "2024-09-03"
