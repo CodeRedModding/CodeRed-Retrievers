a9a24b0292506e8af8694dc208d43c6c1d32a858
BranK <23649425+ItsBranK@users.noreply.github.com>
2025-02-23 17:47:43 -0500

    Initial Commit

--- Public/News.cr
+++ Public/News.cr
@@ -6,7 +6,7 @@
       "category": "Patch Notes",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/rocket-league-world-championship-2025.jpg",
       "author": "Esports News",
-      "date": "2025-02-06"
+      "date": "Feb 6th 2025"
     },
     {
       "title": "Rocket League Patch Notes v2.48",
@@ -14,7 +14,7 @@
       "category": "Patch Notes",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-48.jpg",
       "author": "Laudie Gonzalez",
-      "date": "2025-01-28"
+      "date": "Jan 28th 2025"
     },
     {
       "title": "It’s Official, Not a Leek: Rocket League x Hatsune Miku!",
@@ -22,7 +22,7 @@
       "category": "Game News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/its-official-not-a-leek-rocket-league-x-hatsune-miku.jpg",
       "author": "Psyonix Team",
-      "date": "2025-01-28"
+      "date": "Jan 28th 2025"
     },
     {
       "title": "RLCS Heads to North Carolina for the Raleigh Major; Tickets on General Sale January 22nd",
@@ -30,7 +30,7 @@
       "category": "Esports News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/rlcs-heads-to-north-carolina.jpg",
       "author": "Psyonix Team",
-      "date": "2025-01-18"
+      "date": "Jan 18th 2025"
     },
     {
       "title": "Rocket League Patch Notes v2.47",
@@ -38,7 +38,7 @@
       "category": "Patch Notes",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-47.jpg",
       "author": "Laudie Gonzalez",
-      "date": "2024-12-16"
+      "date": "Dec 16th 2024"
     },
     {
       "title": "RLCS 2025: Updates",
@@ -46,7 +46,7 @@
       "category": "Esports News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/rlcs-2025-updates.jpg",
       "author": "Psyonix Team",
-      "date": "2024-12-15"
+      "date": "Dec 15th 2024"
     },
     {
       "title": "Frosty Fest 2024 Brings Chills and Thrills to Rocket League!",
@@ -54,7 +54,7 @@
       "category": "Game News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/frosty-fest-2024-brings-chills-and-thrills-to-rocket-league.jpg",
       "author": "Psyonix Team",
-      "date": "2024-12-10"
+      "date": "Dec 10th 2024"
     },
     {
       "title": "Patch Notes: Season 17 Live",
@@ -62,7 +62,7 @@
       "category": "Patch Notes",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-season-17-live.jpg",
       "author": "Laudie Gonzalez",
-      "date": "2024-12-04"
+      "date": "Dec 4th 2024"
     },
     {
       "title": "Rocket League Patch Notes v2.46",
@@ -70,7 +70,7 @@
       "category": "Game Update",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-46.jpg",
       "author": "Laudie Gonzalez",
-      "date": "2024-12-03"
+      "date": "Dec 3rd 2024"
     },
     {
       "title": "Season 17 Brings Online Free Play as Rocket League Heads to the Arcades",
@@ -78,7 +78,7 @@
       "category": "Game News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/season-17-brings-online-free-play-as-rocket-league-heads-to-the-arcades.jpg",
       "author": "Psyonix Team",
-      "date": "2024-11-26"
+      "date": "Nov 26th 2024"
     },
     {
       "title": "Collegiate Rocket League Spring 2025 Preview",
@@ -86,7 +86,7 @@
       "category": "Esports News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/collegiate-rocket-league-spring-2025-preview.jpg",
       "author": "Psyonix Team",
-      "date": "2024-11-22"
+      "date": "Nov 22nd 2024"
     },
     {
       "title": "Rocket League S16’s Season Rush Kicks Off!",
@@ -94,7 +94,7 @@
       "category": "Game News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/rocket-league-s16-season-rush-kicks-off.jpg",
       "author": "Psyonix Team",
-      "date": "2024-11-19"
+      "date": "Nov 19th 2024"
     },
     {
       "title": "RLCS Is Back With a Bang For 2025!",
@@ -102,7 +102,7 @@
       "category": "Esports News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/rlcs-is-back-with-a-bang-for-2025.jpg",
       "author": "Psyonix Team",
-      "date": "2024-10-23"
+      "date": "Oct 23rd 2024"
     },
     {
       "title": "Patch Notes v2.45",
@@ -110,7 +110,7 @@
       "category": "Patch Notes",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/patch-notes-v2-45.jpg",
       "author": "Devin Conners",
-      "date": "2024-10-22"
+      "date": "Oct 22nd 2024"
     },
     {
       "title": "Introducing Rocket League Match History and Player Profiles!",
@@ -118,7 +118,7 @@
       "category": "Game News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/introducing-rocket-league-match-history-and-player-profiles.jpg",
       "author": "Psyonix Team",
-      "date": "2024-10-21"
+      "date": "Oct 21st 2024"
     },
     {
       "title": "The 2024 RLCS Offseason Heats Up With More Ways to Compete!",
@@ -126,7 +126,7 @@
       "category": "Esports News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/2024-offseason-tournaments.jpg",
       "author": "Psyonix Team",
-      "date": "2024-10-16"
+      "date": "Oct 16th 2024"
     },
     {
       "title": "It's a Spider-Man Spooktacular in Rocket League's Haunted Hallows!",
@@ -134,7 +134,7 @@
       "category": "Game News",
       "imageUrl": "https://raw.githubusercontent.com/CodeRedModding/CodeRed-Retrievers/refs/heads/main/Assets/News/its-a-spider-man-spooktacular-in-rocket-leagues-haunted-hallows.jpg",
       "author": "Psyonix Team",
-      "date": "2024-10-16"
+      "date": "Oct 16th 2024"
     }
   ]
 }
