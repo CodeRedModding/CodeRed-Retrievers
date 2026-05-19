d80853373d94a3168d887238e7ab3963d7f38140
BranK <23649425+ItsBranK@users.noreply.github.com>
2026-05-19 07:25:41 -0400

    Initial Commit

---
+++ Public/Versions.cr
@@ -0,0 +1,8 @@
+{
+	"signature": "b10756b83ec323480aaaf69eb3134aa125b01f54a9ba455ba5dbdb96187c78e16833db1268578447dac361356619d9fe7d2451518ed67ed20b344b4db017f7f3",
+	"PsyonixVersion": "260506.26700.517210",
+	"DotNetVersion": "10.0",
+	"ModuleVersion": "3.0.2",
+	"PsyonixBuildId ": "-1652286008",
+	"LauncherVersion": "1.4.6"
+}
